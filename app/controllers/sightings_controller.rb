37c5a3a6907d0eb637097b3e74f9e5cd3a1972c7
Chawwh <chachablueftw@gmail.com>
2021-10-26 12:58:36 -0600

    worked halfway through stretch user stories. brain go hurt. much ow

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,4 +1,6 @@
 class SightingsController < ApplicationController
+    
+
     def index
         sightings = Sighting.where(date: params[:start_date]..params[:end_date])
     render json: sightings
--- spec/models/sighting_spec.rb
+++ spec/models/sighting_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Sighting, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'has to have a date' do
+    monke = Sighting.create latitude: "42.5256° N", longitude: "124.6446° W"
+    expect(monke.errors[:date]).to_not be_empty
+  end
+
+  it 'has to have a latitude' do
+    monke = Sighting.create date: "2019-10-26T00:20:19.000Z", longitude: "124.6446° W"
+    expect(monke.errors[:latitude]).to_not be_empty
+  end
+
+  it 'has to have a longitude' do
+    monke = Sighting.create date: "2019-10-26T00:20:19.000Z", latitude: "42.5256° N"
+    expect(monke.errors[:longitude]).to_not be_empty
+  end
+
+
 end
